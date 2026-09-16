9b53f1913aba3983f207df269a397b6bb75c03b6
renderman77 <render.man@libero.it>
2026-09-16 17:56:11 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "it.smartplugmonitor"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "it.smartplugmonitor"
+        minSdk = 25
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+}
