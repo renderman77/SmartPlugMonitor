1d6e4d7aff4384a44b0677de6afadcfa3f7c7ffe
renderman77 <render.man@libero.it>
2026-09-18 11:55:21 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
 plugins {
-    id("com.android.application") version "9.4.0" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
