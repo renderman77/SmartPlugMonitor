aad11f2a2b1e710467345dafc729d6b8f57bc8e0
renderman77 <render.man@libero.it>
2026-09-18 13:00:48 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "1.9.22"
 }
 
 android {
@@ -14,15 +13,10 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
