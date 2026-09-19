cb4144aab532a7cf3d6c14cb2885ecbd0c6fa00e
renderman77 <render.man@libero.it>
2026-09-19 15:58:16 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,14 +14,34 @@ android {
         versionName = "1.0"
     }
 
-    buildTypes {
-        debug {
-            isMinifyEnabled = false
-        }
-        release {
-            isMinifyEnabled = false
+signingConfigs {
+    create("release") {
+        val keystoreFile = System.getenv("SIGNING_KEY_FILE")
+        val keystorePassword = System.getenv("KEYSTORE_PASSWORD")
+        val keyAlias = System.getenv("KEY_ALIAS")
+        val keyPassword = System.getenv("KEY_PASSWORD")
+
+        if (keystoreFile != null && keystorePassword != null &&
+            keyAlias != null && keyPassword != null) {
+
+            storeFile = file(keystoreFile)
+            storePassword = keystorePassword
+            this.keyAlias = keyAlias
+            this.keyPassword = keyPassword
         }
     }
+}
+
+buildTypes {
+    debug {
+        isMinifyEnabled = false
+    }
+
+    release {
+        isMinifyEnabled = false
+        signingConfig = signingConfigs.getByName("release")
+    }
+}
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
